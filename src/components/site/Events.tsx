302d748dec36f49589f110bee420342486d9cc6d
Valerie <109770126+innerphyre@users.noreply.github.com>
2026-08-01 12:38:53 -0700

    Update event dates for upcoming events

--- src/components/site/Events.tsx
+++ src/components/site/Events.tsx
@@ -15,7 +15,7 @@ const events = [
   {
     type: "Chapter Meetup",
     title: "Nevada ISOC and you",
-    date: "August 18, 2026",
+    date: "August 17, 2026",
     time: "6:00 PM PT",
     location: "Zoom Room",
     icon: MapPin,
@@ -25,7 +25,7 @@ const events = [
   {
     type: "Metaweb Summit",
     title: "Desirable Properties Revealed",
-    date: "Sept 16, 2026",
+    date: "Sept 21, 2026",
     time: "4:00 - 6:00 PM PT",
     location: "Hybrid, SF Bay",
     icon: Video,
